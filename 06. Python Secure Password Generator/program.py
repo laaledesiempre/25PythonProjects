398abea97511caf721cc25912b2b11f09a0e9305
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-05-19 14:15:01 -0300

    Password generator base

---
+++ 06. Python Secure Password Generator/program.py
@@ -0,0 +1,9 @@
+import string
+import secrets
+
+letter= string.ascii_letters
+numbers= string.digits
+symbols= string.punctuation
+
+class Password(password_length=12,):
+    length= password_length
