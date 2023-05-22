04730718eab8620d438001754b7bded34d9993f2
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-05-22 13:59:24 -0300

    Password generator completed

--- 06. Python Secure Password Generator/program.py
+++ 06. Python Secure Password Generator/program.py
@@ -1,9 +1,37 @@
 import string
 import secrets
 
-letter= string.ascii_letters
+letters= string.ascii_letters
 numbers= string.digits
 symbols= string.punctuation
 
-class Password(password_length=12,):
-    length= password_length
+alphabeth= letters+numbers+symbols
+
+def generate_password(leng=12):
+    print("This password will be show, but no stored in the program. copy and clean the console for more security")
+    password=""
+    for i in range(leng):
+        password+= secrets.choice(alphabeth)
+    return password
+
+while True:
+
+    while True:
+        passpromt=input("Select the password length or press ENTER")
+        if passpromt=="":
+            break
+        else:
+            try:
+                passpromt=int(passpromt)
+                break
+            except:
+                print("non valid length")
+                continue
+    if passpromt:
+        print(generate_password(passprompt))
+    else:
+        print(generate_password())
+    print("PASSWORD GENERATED PRESS ENTER TO MAKE MORE OR WRITE exit")
+    response= input()
+    if response=="exit":
+        break
