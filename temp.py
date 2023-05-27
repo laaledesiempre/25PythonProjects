aed1280b72114539b34ce5c289f72bb064f04fb7
Alejandra Botacura <alejandrabotacura@outlook.com.ar>
2023-05-27 13:03:52 -0300

    count return array

--- temp.py
+++ temp.py
@@ -8,12 +8,13 @@ import random
 #[print(dices) for dices in [random.choice(range(1,7)) for i in range(5)]]
 list=[random.choice(range(1,7)) for i in range(5)]
 print(list)
-[print(ocurrences) for ocurrences in ]
-ocurrences=[]
-for i in range(1,7):
-    if list.count(i) > 1:
-        ocurrences.append(list.count(i))
-print(ocurrences)
+result= [list.count(i) for i in range(1,7) if list.count(i) > 1]
+print(result)
+#ocurrences=[]
+#for i in range(1,7):
+#    if list.count(i) > 1:
+#        ocurrences.append(list.count(i))
+#print(ocurrences)
 
     
 
