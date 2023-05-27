c9ffb408963eb69d59b82700e90dbd3e30ed7a50
Alejandra Botacura <alejandrabotacura@outlook.com.ar>
2023-05-27 13:15:16 -0300

    returns an array of occurences

--- temp.py
+++ temp.py
@@ -6,10 +6,12 @@ import random
 #    print(rolldice())
     
 #[print(dices) for dices in [random.choice(range(1,7)) for i in range(5)]]
-list=[random.choice(range(1,7)) for i in range(5)]
-print(list)
-result= [list.count(i) for i in range(1,7) if list.count(i) > 1]
+#list=[random.choice(range(1,7)) for i in range(5)]
+#print(list)
+result= [[list.count(i) for i in range(1,7) if list.count(i) > 1] for list in [[random.choice(range(1,7)) for j in range(5)]]]
 print(result)
+#(list.count(i) for i in range(1,7) if list.count(i) > 1)
+
 #ocurrences=[]
 #for i in range(1,7):
 #    if list.count(i) > 1:
